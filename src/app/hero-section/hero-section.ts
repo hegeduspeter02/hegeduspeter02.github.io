26cf888a257a0002a9fcb9191178b199620fce0a
Hegedűs Péter <hegipeti25@gmail.com>
2026-06-15 06:32:49 +0200

    refactor: simplify hero section component and enhance typed.js integration

--- src/app/hero-section/hero-section.ts
+++ src/app/hero-section/hero-section.ts
@@ -1,10 +1,4 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnDestroy,
-  ViewChild,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
@@ -20,35 +14,13 @@ import Typed from 'typed.js';
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.css',
 })
-export class HeroSection implements AfterViewInit, OnDestroy {
-  @ViewChild('typedText', {
-    static: false,
-  })
-  typedTextEl?: ElementRef<HTMLElement>;
-  private typed?: Typed;
-
-  ngAfterViewInit() {
-    if (!this.typedTextEl) {
-      return;
-    }
-
-    this.typed = new Typed(this.typedTextEl.nativeElement, {
+export class HeroSection implements OnInit {
+  ngOnInit() {
+    new Typed('.typed-element', {
       strings: [
-        'szoftverfejlesztő',
+        'szoftverfejlesztő.',
       ],
       typeSpeed: 70,
-      showCursor: true,
-      cursorChar: '|',
-      backSpeed: 0,
-      smartBackspace: false,
-      loop: false,
-      onComplete: () => {
-        this.typed?.cursor?.classList.remove('typed-cursor');
-      },
     });
-  }
-
-  ngOnDestroy() {
-    this.typed?.destroy();
   }
 }
